153196b3ed63e026b582d830fe537d13aa9cda4b
SSto <95592688+sstover25@users.noreply.github.com>
2022-01-27 13:19:11 -0600

    added drag and drop within and between columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,6 +174,57 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// makes lists sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"), 
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text, 
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
